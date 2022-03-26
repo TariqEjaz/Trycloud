7d5b95c36d1ae1d18e5df0c8fbd6a7a5ea2217d6
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-03-25 23:18:11 -0400

    PageFact updated

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,4 +1,11 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
