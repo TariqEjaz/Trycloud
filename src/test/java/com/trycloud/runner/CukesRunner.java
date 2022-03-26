9f6ac73e44966d91f927b82a772cf02d04e1eff4
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-03-25 22:20:49 -0400

    Sharing project

---
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -0,0 +1,19 @@
+package com.trycloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+       features = "src/test/resources/features",
+       glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+
+)
+public class CukesRunner {
+
+
+}
